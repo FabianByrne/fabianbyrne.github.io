e8df0057a6b898e8b173d4281a267107d8b023af
FabianByrne <127799518+FabianByrne@users.noreply.github.com>
2023-11-01 16:34:40 +0000

    Add files via upload

---
+++ homejavascript.js
@@ -0,0 +1,33 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+// Welcome banner effects
+
+function welcomelight() {
+    var banner = document.getElementById("welcomebanner");
+    banner.style.borderColor = "yellow";
+    banner.style.fontSize = "19px";
+
+}
+
+function welcomelightout() {
+    var banner = document.getElementById("welcomebanner");
+    banner.style.borderColor = "black";
+    banner.style.fontSize = "18px";
+
+}
+
+// nav bar list effects
+
+
+function lightup(element) {
+    element.style.borderColor = "yellow";
+}
+
+function lightout(element) {
+    element.style.borderColor = "black";
+}
+
+
